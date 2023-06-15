2cfcab485076e3d07347f30bf72ff310c5fa2c87
Emilia2022 <106738248+Emilia2022@users.noreply.github.com>
2023-06-15 10:41:40 +0300

    service

---
+++ src/main/java/com/example/managedlicensins/service/LicenseService.java
@@ -0,0 +1,53 @@
+package com.example.managedlicensins.service;
+
+import lombok.RequiredArgsConstructor;
+import model.License;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class LicenseService {
+
+        private final MessageSource messages;
+        public License getLicense(String licenseId, String organizationId) {
+            License license = new License();
+            license.setId(new Random().nextLong());
+            license.setLicenseId(licenseId);
+            license.setOrganizationId(organizationId);
+            license.setDescription("Software product");
+            license.setProductName("Ostock");
+            license.setLicenseType("full");
+            return license;
+        }
+
+        public String createLicense(License license, String organizationId, Locale locale) {
+            String responseMessage = null;
+            if (license != null) {
+                license.setOrganizationId(organizationId);
+                responseMessage = String.format(messages.getMessage(
+                        "license.create.message", null, locale), license);
+            }
+            return responseMessage;
+        }
+
+        public String deleteLicense(String licenseId, String organizationId) {
+            String responseMessage;
+            responseMessage = String.format(
+                    "Deleting license with id %s for the organization %s", licenseId, organizationId);
+            return responseMessage;
+
+        }
+        public String updateLicense(License license, String organizationId) {
+            String responseMessage = null;
+            if (license != null) {
+                license.setOrganizationId(organizationId);
+                responseMessage = String.format(messages.getMessage("license.update.message", null, null), license);
+            }
+            return responseMessage;
+        }
+
+}
---
+++ src/test/java/com/example/managedlicensins/ManagedLicensinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managedlicensins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagedLicensinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
